0b9580cc91e208358fc04c5ade28bbd018739020
TRochaDev <trochadev@gmail.com>
2022-04-01 19:02:07 -0300

    Cria pasta de arquivos do microservice-b

---
+++ microservice-b/src/test/java/br/com/trochadev/cotacaomoedas/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.trochadev.cotacaomoedas;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
