ea7a9d2bbee52db9efda7152c3f2d33837a10614
TRochaDev <trochadev@gmail.com>
2022-04-02 15:06:48 -0300

    Cria HolidayClient.

--- microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/client/HolidayClient.java
+++ microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/client/HolidayClient.java
@@ -14,6 +14,6 @@ public interface HolidayClient {
     @GET
     @Path("/{year}")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Holiday> getHolidays(@PathParam ("year")String year, @QueryParam("token") String token);
+    List<Holiday> getHolidays(@PathParam ("year")String year, @QueryParam("token") String token);
 
 }
