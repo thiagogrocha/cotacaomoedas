c5811d269d2eac8dfd7a1239a811755b53f91382
TRochaDev <trochadev@gmail.com>
2022-04-02 15:03:01 -0300

    Remove classe não utilizadas

--- microservice-b/src/test/java/br/com/trochadev/cotacaomoedas/GreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package br.com.trochadev.cotacaomoedas;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
