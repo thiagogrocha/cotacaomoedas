9f93a26c7a8b8e29140eaf20a6cfe9bd30195263
TRochaDev <trochadev@gmail.com>
2022-04-02 15:07:53 -0300

    Cria QuotationController.

---
+++ microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/controller/QuotationController.java
@@ -0,0 +1,40 @@
+package br.com.trochadev.cotacaomoedas.controller;
+
+import br.com.trochadev.cotacaomoedas.entity.Quotation;
+import br.com.trochadev.cotacaomoedas.exception.QuotationNotFoundException;
+import br.com.trochadev.cotacaomoedas.exceptionhandler.ExceptionHandler;
+import br.com.trochadev.cotacaomoedas.service.QuotationService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.inject.Inject;
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("CotacaoDolar")
+@Tag(name = "Cotação", description = "Dólar compra/venda")
+public class QuotationController {
+
+    @Inject
+    QuotationService service;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Cotação Dólar por dia", description = "Retorna a cotação do Dólar no dia informado")
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Sucesso",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = Quotation.class))),
+            @APIResponse(responseCode = "403", description = "Cotação não localizada",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON, schema = @Schema(implementation = ExceptionHandler.ErrorResponseBody.class)))})
+    public Quotation getCotacao(@BeanParam ParameterIn data) throws QuotationNotFoundException {
+        return service.getQuotation(data);
+    }
+
+}
