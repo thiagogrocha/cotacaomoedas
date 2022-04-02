055c1d8f60847599416202362a015a82dee31bf5
TRochaDev <trochadev@gmail.com>
2022-04-02 11:49:50 -0300

    Adiciona anotação swagger.

--- microservice-a/src/main/java/br/com/trochadev/cotacaomoedas/entity/Cotacao.java
+++ microservice-a/src/main/java/br/com/trochadev/cotacaomoedas/entity/Cotacao.java
@@ -1,9 +1,13 @@
-package br.com.trochadev.cotacaomoedas.model;
+package br.com.trochadev.cotacaomoedas.entity;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import javax.json.bind.annotation.JsonbProperty;
 import java.util.List;
 
+@Schema(name="Cotação", description="Classe que representa a Cotação")
 public class Cotacao {
+
     @JsonbProperty(value = "@odata.context")
     String context;
     List<CotacaoDolar> value;
