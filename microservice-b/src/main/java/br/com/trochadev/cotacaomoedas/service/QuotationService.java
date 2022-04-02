abd5b1f3b674597d121130c4dc8677e5e45e0510
TRochaDev <trochadev@gmail.com>
2022-04-02 20:35:54 -0300

    Cria processo que retorna duas cotações.

--- microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/service/QuotationService.java
+++ microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/service/QuotationService.java
@@ -29,10 +29,20 @@ public class QuotationService {
     @CacheResult(cacheName = "quotation")
     public Quotation getQuotation(ParameterIn data) throws QuotationNotFoundException {
         String date = data.getData().orElse("'" + LocalDate.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy")) + "'");
+        String date2 = date;
 
         bdService.setDate(date);
         if (bdService.isBusinessDay()) {
-            return client.getQuotation(date);
+            Quotation quotation = client.getQuotation(date);
+
+            do {
+                date2 = LocalDate.parse(date2.replace("'", ""), DateTimeFormatter.ofPattern("MM-dd-yyyy")).minusDays(1).format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+                bdService.setDate(date2);
+
+            } while (!bdService.isBusinessDay());
+            quotation.getValue().add(client.getQuotation("'" + date2 + "'").getValue().get(0));
+
+            return quotation;
         } else
             throw new QuotationNotFoundException("A data informada não corresponde a um dia útil! " + bdService.getDay());
     }
