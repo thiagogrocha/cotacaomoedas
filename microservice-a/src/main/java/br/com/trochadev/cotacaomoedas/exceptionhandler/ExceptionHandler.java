4a17abcf992aeaf771d987f6b84b7467c6654dda
TRochaDev <trochadev@gmail.com>
2022-04-02 12:08:15 -0300

    Implementa ExceptionHandler

---
+++ microservice-a/src/main/java/br/com/trochadev/cotacaomoedas/exceptionhandler/ExceptionHandler.java
@@ -0,0 +1,36 @@
+package br.com.trochadev.cotacaomoedas.exceptionhandler;
+
+import br.com.trochadev.cotacaomoedas.exception.QuotationNotFoundException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class ExceptionHandler implements ExceptionMapper<Exception> {
+
+    @Override
+    public Response toResponse(Exception exception) {
+        if(exception instanceof QuotationNotFoundException) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity(new ErrorResponseBody(exception.getMessage()))
+                    .build();
+        }
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(new ErrorResponseBody("Erro interno. Tente novamente"))
+                .build();
+    }
+
+    public static final class ErrorResponseBody {
+
+        private final String message;
+
+        public ErrorResponseBody(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
