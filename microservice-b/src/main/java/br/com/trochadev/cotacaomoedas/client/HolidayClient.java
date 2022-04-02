efc41d49a4831a26b449717d71922e21a576adb1
TRochaDev <trochadev@gmail.com>
2022-04-02 19:41:24 -0300

    Identa linhas.

--- microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/client/HolidayClient.java
+++ microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/client/HolidayClient.java
@@ -14,6 +14,6 @@ public interface HolidayClient {
     @GET
     @Path("/{year}")
     @Produces(MediaType.APPLICATION_JSON)
-    List<Holiday> getHolidays(@PathParam ("year")String year, @QueryParam("token") String token);
+    List<Holiday> getHolidays(@PathParam("year") String year, @QueryParam("token") String token);
 
 }
