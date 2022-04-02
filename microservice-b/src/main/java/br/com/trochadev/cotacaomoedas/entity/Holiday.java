357758dec6e5f7958eee5953a7d4674d9d2c2054
TRochaDev <trochadev@gmail.com>
2022-04-02 13:27:47 -0300

    Cria classe Holiday

---
+++ microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/entity/Holiday.java
@@ -0,0 +1,40 @@
+package br.com.trochadev.cotacaomoedas.entity;
+
+public class Holiday {
+    private String date;
+    private String name;
+    private String type;
+    private String level;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
