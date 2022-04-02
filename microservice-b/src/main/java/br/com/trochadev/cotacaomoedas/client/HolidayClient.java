fb8ced050e92081049649d1117e8bf1d1e84d1f0
TRochaDev <trochadev@gmail.com>
2022-04-02 13:28:00 -0300

    Cria classe HolidayClient

---
+++ microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/client/HolidayClient.java
@@ -0,0 +1,19 @@
+package br.com.trochadev.cotacaomoedas.client;
+
+import br.com.trochadev.cotacaomoedas.entity.Holiday;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@RegisterRestClient(configKey = "holiday")
+@Path("/holidays")
+public interface HolidayClient {
+
+    @GET
+    @Path("/{year}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Holiday> getHolidays(@PathParam ("year")String year, @QueryParam("token") String token);
+
+}
