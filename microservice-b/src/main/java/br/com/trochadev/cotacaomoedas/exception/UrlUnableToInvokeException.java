dd4cf2d6b22ae370e3a4bec1a0730ded24d2954f
TRochaDev <trochadev@gmail.com>
2022-04-03 00:15:36 -0300

    Cria UrlUnableToInvokeException .

---
+++ microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/exception/UrlUnableToInvokeException.java
@@ -0,0 +1,7 @@
+package br.com.trochadev.cotacaomoedas.exception;
+
+public class UrlUnableToInvokeException extends Exception{
+    public UrlUnableToInvokeException(String message) {
+        super(message);
+    }
+}
