f4e28e71df433b1ddb65ddf58c93f9aec177a6cf
TRochaDev <trochadev@gmail.com>
2022-04-03 00:15:00 -0300

    Implementa tratativa de exceções .

--- microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/service/QuotationService.java
+++ microservice-b/src/main/java/br/com/trochadev/cotacaomoedas/service/QuotationService.java
@@ -4,6 +4,7 @@ import br.com.trochadev.cotacaomoedas.client.QuotationClient;
 import br.com.trochadev.cotacaomoedas.controller.ParameterIn;
 import br.com.trochadev.cotacaomoedas.entity.Quotation;
 import br.com.trochadev.cotacaomoedas.exception.QuotationNotFoundException;
+import br.com.trochadev.cotacaomoedas.exception.UrlUnableToInvokeException;
 import io.quarkus.cache.CacheResult;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.slf4j.Logger;
@@ -27,21 +28,24 @@ public class QuotationService {
     BusinessDayService bdService;
 
     @CacheResult(cacheName = "quotation")
-    public Quotation getQuotation(ParameterIn data) throws QuotationNotFoundException {
+    public Quotation getQuotation(ParameterIn data) throws QuotationNotFoundException, UrlUnableToInvokeException {
         String date = data.getData().orElse("'" + LocalDate.now().format(DateTimeFormatter.ofPattern("MM-dd-yyyy")) + "'");
         String date2 = date;
+        Quotation quotation = null;
 
         bdService.setDate(date);
         if (bdService.isBusinessDay()) {
-            Quotation quotation = client.getQuotation(date);
+            try {
+                quotation = client.getQuotation(date);
+                do {
+                    date2 = LocalDate.parse(date2.replace("'", ""), DateTimeFormatter.ofPattern("MM-dd-yyyy")).minusDays(1).format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
+                    bdService.setDate(date2);
 
-            do {
-                date2 = LocalDate.parse(date2.replace("'", ""), DateTimeFormatter.ofPattern("MM-dd-yyyy")).minusDays(1).format(DateTimeFormatter.ofPattern("MM-dd-yyyy"));
-                bdService.setDate(date2);
-
-            } while (!bdService.isBusinessDay());
-            quotation.getValue().add(client.getQuotation("'" + date2 + "'").getValue().get(0));
-
+                } while (!bdService.isBusinessDay());
+                quotation.getValue().add(client.getQuotation("'" + date2 + "'").getValue().get(0));
+            } catch (Exception e) {
+                throw new UrlUnableToInvokeException(e.getMessage());
+            }
             return quotation;
         } else
             throw new QuotationNotFoundException("A data informada não corresponde a um dia útil! " + bdService.getDay());
